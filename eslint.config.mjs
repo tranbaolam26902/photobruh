78538f407547235632a64a3fd408683b4758c713
campbells <tranbaolam26902@gmail.com>
2026-03-26 14:36:51 +0700

    chore(repo): add prettier plugins to sort imports and tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import prettierConfig from 'eslint-config-prettier';
-import eslintPluginImport from 'eslint-plugin-import';
 import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,27 +12,9 @@ const eslintConfig = defineConfig([
   {
     plugins: {
       prettier: prettierPlugin,
-      import: eslintPluginImport,
     },
     rules: {
       'prettier/prettier': 'warn',
-      'import/order': [
-        'warn',
-        {
-          groups: [
-            'builtin',
-            'external',
-            'internal',
-            'parent',
-            'sibling',
-            'index',
-          ],
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
-        },
-      ],
     },
   },
 
